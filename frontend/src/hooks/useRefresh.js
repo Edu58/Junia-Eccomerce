f254294c33a14e8b82be2bbc20d17c837bea714f
“Edu58” <edumuriithi58@gmail.com>
2022-09-09 14:20:06 +0300

    frontend: decode token in useRefresh hook

--- frontend/src/hooks/useRefresh.js
+++ frontend/src/hooks/useRefresh.js
@@ -1,16 +1,24 @@
+import jwt_decode from "jwt-decode";
 import { useContext } from "react";
 import ProductsContext from "../context/products";
 import axiosClient from "../components/Axios";
 
 const useRefreshToken = () => {
-    const { setAuthToken } = useContext(ProductsContext)
+    const { setAuthToken, cartDispatch } = useContext(ProductsContext)
 
     const refresh = async () => {
         const response = await axiosClient.get('/auth/refresh', {
             withCredentials: true
         })
 
-        setAuthToken(response.data?.accessToken)
+        setAuthToken(response?.data?.accessToken)
+
+        const userInfo = jwt_decode(response?.data?.accessToken).userInfo
+
+        cartDispatch({
+            type: "USER_LOGIN",
+            payload: userInfo
+        })
 
         return response.data?.accessToken
     }
