5ca0bdb68b1a445485a97ddc558952a9cae0a46d
“Edu58” <edumuriithi58@gmail.com>
2022-09-10 00:28:24 +0300

    backend: add orderModel

---
+++ backend/models/orderModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    orderItems: [
+        {
+            title: { type: String, required: true },
+            price: { type: Number, required: true },
+            quantity: { type: Number, required: true },
+            image: { type: String, required: true },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            }
+        }
+    ],
+    shippingAddress: {
+        fullName: { type: String, required: true },
+        country: { type: String, required: true },
+        city: { type: String, required: true },
+        address: { type: String, required: true },
+        postalCode: { type: String, required: true }
+    },
+    paymentMethod: { type: String, required: true },
+    paymentResult: {
+        id: String,
+        status: String,
+        update_time: String,
+        email: String
+    },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    isPaid: { type: Boolean, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, default: false },
+    deliveredAt: { type: Date }
+},
+    {
+        timestamps: true
+    }
+)
+
+module.exports = mongoose.model('Order', orderSchema)
