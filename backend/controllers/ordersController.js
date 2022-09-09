8a131b4bb7afd3a9c878bf7f1f782a0b222181f3
“Edu58” <edumuriithi58@gmail.com>
2022-09-10 00:41:41 +0300

    backend: add ordersController

---
+++ backend/controllers/ordersController.js
@@ -0,0 +1,26 @@
+const Order = require('../models/orderModel')
+
+const createOrder = async (req, res) => {
+
+    const data = req.body
+
+    try {
+        const newOrder = await new Order({
+            orderItems: data.orderItems.map(x => ({ ...x, product: x._id })),
+            shippingAddress: data.shippingAddress,
+            shippingPrice: data.shippingPrice,
+            paymentMethod: data.paymentMethod,
+            itemsPrice: data.itemsPrice,
+            taxPrice: data.taxPrice,
+            totalPrice: data.totalPrice,
+            user: req.user._id
+        })
+
+        const order = await newOrder.save()
+        res.status(201).json({ "message": "order created", Order })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports = createOrder
--- backend/routes/store.js
+++ backend/routes/store.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const verifyJWT = require('../middleware/verifyJWT')
+const createOrder = require('../controllers/ordersController')
 const { getAllProducts, getProductsCategories, addProduct } = require('../controllers/productsController')
 
 router.get('/products/categories', getProductsCategories)
 router.get('/products', getAllProducts)
-router.post('/products', addProduct)
+router.post('/products', verifyJWT, addProduct)
+router.post('/orders', verifyJWT, createOrder)
 
 module.exports = router
